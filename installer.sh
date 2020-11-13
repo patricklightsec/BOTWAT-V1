20b31829eec97b89ca6b09818e68385ee23b5d70
Aditya Rahmatullah <74150104+patricklightsec@users.noreply.github.com>
2020-11-14 01:35:49 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 clear
 
-echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list
 sudo wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
 sudo apt update -y
 sudo apt upgrade -y 
-apt -y install kali-linux-large -y 
-apt-get install kali-linux-all -y
+sudo apt -y install kali-linux-large -y 
+sudo apt-get install kali-linux-all -y
+sudo apt-get install -y pacman
+sudo curl -O https://blackarch.org/strap.sh
+sudo chmod +x strap.sh
+sudo ./strap.sh
+sudo pacman -Sg | grep blackarch
+sudo pacman -Sgg | grep blackarch-wireless
+sudo pacman -S blackarch-category
+sudo pacman -S package-name
+
+exit;
