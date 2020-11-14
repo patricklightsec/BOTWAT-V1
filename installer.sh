d57281541d008d6ab847b6a471f2830c2a09a029
Aditya Rahmatullah <74150104+patricklightsec@users.noreply.github.com>
2020-11-15 02:18:25 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,8 +3,11 @@ clear
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install kali-linux-default
-sudo apt-get install parrot-tools
+sudo apt install kali-linux-default -y
+sudo npm install -f
+sudo npm audit fix -f
+sudo apt-get install build-essential libssl-dev -y
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 sudo pip install hacking
 sudo pip install h8mail
 sudo apt install hydra
