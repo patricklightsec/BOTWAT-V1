498e21f79c93439460639091147a01fb1c42723e
Aditya Rahmatullah <74150104+patricklightsec@users.noreply.github.com>
2020-11-15 02:04:21 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 clear
 
-sudo echo "deb http://http.kali.org/kali kali-experimental main non-free contrib" | sudo tee -a /etc/apt/sources.list
-sudo wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
 sudo apt update -y
-sudo apt upgrade -y 
-sudo apt -y install kali-linux-large -y 
-sudo apt-get install kali-linux-all -y
-sudo pacman -S package-name
+sudo apt upgrade -y
+sudo apt install kali-linux-default
 sudo apt-get install parrot-tools
 sudo pip install hacking
 sudo pip install h8mail
